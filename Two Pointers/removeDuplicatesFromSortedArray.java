dadd75240335eaa55f2ece7ca644d4f16beadc90
Madhura Walawalkar <madhurasdw@gmail.com>
2026-06-03 10:56:50 +0530

    Implement removeDuplicates method in Solution class

---
+++ Two Pointers/removeDuplicatesFromSortedArray.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int s=0;
+        for(int f=1;f<nums.length;f++){
+            if(nums[s]!=nums[f]){
+                s++;
+                nums[s]=nums[f];
+            }
+        }
+        return s+1;
+    }
+}
