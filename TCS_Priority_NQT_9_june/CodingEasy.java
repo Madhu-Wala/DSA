9880d6dc78a437c03bd066169a6b31ee26882f50
Madhura Walawalkar <madhurasdw@gmail.com>
2026-06-11 14:32:45 +0530

    Create CodingEasy.java

---
+++ TCS_Priority_NQT_9_june/CodingEasy.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class CodingEasy {
+    public static int firstOcc(int[] arr,int target){
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]==target){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int target=sc.nextInt();
+        int firstocc=firstOcc(arr,target);
+        System.out.println(firstocc);
+    }
+}
