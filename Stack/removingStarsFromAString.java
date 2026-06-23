cbdeaff0ec160d2a88167e41ee05af6152a9ee13
Madhura Walawalkar <madhurasdw@gmail.com>
2026-06-23 11:28:30 +0530

    Create removingStarsFromAString.java

---
+++ Stack/removingStarsFromAString.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String removeStars(String s) {
+        Stack<Character> st=new Stack<>();
+        for(char c: s.toCharArray()){
+            if(c!='*'){
+                st.push(c);
+            }else{
+                st.pop();
+            }
+        }
+        StringBuilder sb=new StringBuilder();
+        while(!st.isEmpty()){
+            sb.append(st.pop());
+        }
+        String ans=sb.reverse().toString();
+        return ans;
+    }
+}
