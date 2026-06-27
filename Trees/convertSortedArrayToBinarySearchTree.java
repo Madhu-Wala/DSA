75ad9b3a7ee6881c62b68339838d198ca9c46bf3
Madhura Walawalkar <madhurasdw@gmail.com>
2026-06-27 23:51:51 +0530

    Implement sorted array to BST conversion

---
+++ Trees/convertSortedArrayToBinarySearchTree.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return createNode(nums,0,nums.length-1);
+    }
+    private TreeNode createNode(int[] nums,int start,int end){
+        if(start>end){
+            return null;
+        }
+        if(start==end){
+            return new TreeNode(nums[start]);
+        }
+        int mid=start+(end-start)/2;
+        TreeNode root =new TreeNode(nums[mid]);
+        root.left=createNode(nums,start,mid-1);
+        root.right=createNode(nums,mid+1,end);
+        return root;
+    }
+}
