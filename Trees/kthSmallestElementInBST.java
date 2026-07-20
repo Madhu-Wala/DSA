1c4c639e83faf9e160003b7e5fa060a0b98deef2
Madhura Walawalkar <madhurasdw@gmail.com>
2026-07-20 20:35:22 +0530

    Implement kth smallest element in BST using inorder

---
+++ Trees/kthSmallestElementInBST.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int result=-1;
+    public int count=0;
+    public void inorder(TreeNode root, int k){
+        if(root==null) return;
+        inorder(root.left,k);
+        count++;
+        if(count==k){
+            result=root.val;
+            return;
+        }
+        inorder(root.right,k);
+    }
+    public int kthSmallest(TreeNode root, int k) {
+        //inorder traversal
+        inorder(root,k);
+        return result;
+    }
+}
